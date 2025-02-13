2494b9e22be249ebfc85ed663d52f3fbf206334d
Victor Diego <studyvictordiego@gmail.com>
2025-02-13 08:31:59 -0300

    Service Initial

--- src/main/java/com/victor/first_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/victor/first_spring_app/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.victor.first_spring_app.controller;
 
+import com.victor.first_spring_app.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello-world")
 
+
 public class HelloWorldController {
+    @Autowired
+    private HelloWorldService helloWorldService;
+
     @GetMapping("/get")
     public String helloWorld(){
-        return "Hello World";
+        return helloWorldService.helloWorld("Victor");
     }
 
 }

bd086f3330c83613f812c5a3b42f607855d49df1
Victor Diego <studyvictordiego@gmail.com>
2025-02-13 07:49:54 -0300

    First Commit

---
+++ src/main/java/com/victor/first_spring_app/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.victor.first_spring_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+
+public class HelloWorldController {
+    @GetMapping("/get")
+    public String helloWorld(){
+        return "Hello World está funcionando";
+    }
+
+}
